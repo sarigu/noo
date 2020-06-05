688f9d5db3a931fc83d096f91ac6c675a4dac4ec
sarigu <32572115+sarigu@users.noreply.github.com>
2020-06-05 03:19:19 +0200

    added animated reviews

--- app.js
+++ app.js
@@ -6,3 +6,19 @@ function openMenu() {
     menu.style.display = "block";
   }
 }
+
+// slightly modified. as seen on https://css-tricks.com/animate-text-on-scroll/
+
+const textPath = document.querySelector("#first-review");
+const secondTextPath = document.querySelector("#second-review");
+
+const h = document.documentElement,
+  b = document.body,
+  st = "scrollTop",
+  sh = "scrollHeight";
+
+document.addEventListener("scroll", e => {
+  let percent = ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100;
+  textPath.setAttribute("startOffset", -percent * 40 + 2700);
+  secondTextPath.setAttribute("startOffset", -percent * 40 + 3200);
+});
